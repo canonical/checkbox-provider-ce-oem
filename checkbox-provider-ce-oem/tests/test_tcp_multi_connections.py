6b84e67658a2e17a7295608048c6f031db656c94
rickwu4444 <rick.wu@canonical.com>
2023-12-19 15:52:28 +0800

    Add unit test for PortOutputer function

--- checkbox-provider-ce-oem/tests/test_tcp_multi_connections.py
+++ checkbox-provider-ce-oem/tests/test_tcp_multi_connections.py
@@ -1,17 +1,79 @@
 #!/usr/bin/python3
 
 import unittest
-import sys
-import io
+from datetime import timedelta
 import tcp_multi_connections
-from unittest import mock
 
 
 class TestPortOutputer(unittest.TestCase):
-    @mock.patch('tcp_multi_connections.PortOutputer',
-                return_value=None)
-    def setUp(self, mock_portoutputer):
-        suppress_text = io.StringIO()
-        sys.stdout = suppress_text
-        self.port_outputer = tcp_multi_connections.PortOutputer()
+    """
+    Unit test for PortOutputer
+    """
 
+    def test_generate_result_pass(self):
+        """
+        Test if port test pass.
+        """
+        list_status = {
+            0: {'time': timedelta(seconds=5), 'status': True},
+            1: {'time': timedelta(seconds=10), 'status': True},
+            2: {'time': timedelta(seconds=3), 'status': True},
+        }
+        port_outputer = tcp_multi_connections.PortOutputer(
+            port=123,
+            list_status=list_status)
+
+        result = port_outputer.generate_result()
+        self.assertEqual(result['port'], 123)
+        self.assertEqual(result['status'], 'PASS')
+        self.assertEqual(result['message'], 'Received payload correct!')
+        self.assertEqual(result['total_period'],
+                         timedelta(seconds=18))
+        self.assertEqual(result['avg_period'], timedelta(seconds=6))
+        self.assertEqual(result['max_period'], timedelta(seconds=10))
+        self.assertEqual(result['min_period'], timedelta(seconds=3))
+
+    def test_generate_result_fail(self):
+        """
+        Test if port test fail.
+        """
+        list_status = {
+            0: {'time': timedelta(seconds=5), 'status': True},
+            1: {'time': timedelta(seconds=10), 'status': False},
+            2: {'time': timedelta(seconds=3), 'status': True},
+        }
+        port_outputer = tcp_multi_connections.PortOutputer(
+            port=123,
+            list_status=list_status)
+
+        result = port_outputer.generate_result()
+        self.assertEqual(result['port'], 123)
+        self.assertEqual(result['status'], 'FAIL')
+        self.assertEqual(result['message'], 'Received payload incorrect!')
+        self.assertEqual(result['total_period'],
+                         timedelta(seconds=18))
+        self.assertEqual(result['avg_period'], timedelta(seconds=6))
+        self.assertEqual(result['max_period'], timedelta(seconds=10))
+        self.assertEqual(result['min_period'], timedelta(seconds=3))
+
+    def test_generate_result_error(self):
+        """
+        Test if port test with error.
+        """
+        port_outputer = tcp_multi_connections.PortOutputer(
+            port=123,
+            message="Connection error!"
+            )
+
+        result = port_outputer.generate_result()
+        self.assertEqual(result['port'], 123)
+        self.assertEqual(result['status'], 'ERROR')
+        self.assertEqual(result['message'], 'Connection error!')
+        self.assertEqual(result['total_period'], None)
+        self.assertEqual(result['avg_period'], None)
+        self.assertEqual(result['max_period'], None)
+        self.assertEqual(result['min_period'], None)
+
+
+if __name__ == '__main__':
+    unittest.main()
