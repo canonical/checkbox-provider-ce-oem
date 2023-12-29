86d24462dbb43ded76327c64da49a6361982bb17
rickwu4444 <rick.wu@canonical.com>
2023-12-29 16:54:22 +0800

    Fix format_output function

--- checkbox-provider-ce-oem/bin/tcp_multi_connections.py
+++ checkbox-provider-ce-oem/bin/tcp_multi_connections.py
@@ -43,57 +43,55 @@ def sorting_data(dict_status):
 
 
 def format_output(port, message="", dict_status={}):
-    status = StatusEnum.SUCCESS
+    result = {
+        'port': port,
+        'status': StatusEnum.SUCCESS,
+        'message': None,
+        'fail': None,
+        'total_period': None,
+        'avg_period': None,
+        'max_period': None,
+        'min_period': None,
+    }
     times = records = None
     if message:
-        status = StatusEnum.ERROR
+        result['status'] = StatusEnum.ERROR
+        result['message'] = message
     elif dict_status:
         times, records = sorting_data(dict_status)
         if records:
-            status = StatusEnum.FAIL
-            message = "Received payload incorrect!"
+            result['fail'] = records
+            result['status'] = StatusEnum.FAIL
+            result['message'] = "Received payload incorrect!"
         else:
-            message = "Received payload correct!"
-
-    return {
-        'port': port,
-        'status': status,
-        'message': message,
-        'fail': records if records else None,
-        'total_period': sum(times, timedelta()) if times else None,
-        'avg_period': sum(times, timedelta())/len(times) if times else None,
-        'max_period': max(times) if times else None,
-        'min_period': min(times) if times else None,
-    }
+            result['message'] = "Received payload correct!"
+        result['total_period'] = sum(times, timedelta())
+        result['avg_period'] = sum(times, timedelta())/len(times)
+        result['max_period'] = max(times)
+        result['min_period'] = min(times)
+    return result
 
 
-def generate_result(results, run_time):
+def check_result(results):
     final = 0
-    for ports in results:
-        if ports['status'] == StatusEnum.FAIL:
+    for port in results:
+        if port['status'] == StatusEnum.FAIL:
             final = 1
-            logging.error("Fail on port {}.\n"
-                          "{}\n"
-                          "Detail:\n{}"
-                          .format(ports['port'],
-                                  ports['message'],
-                                  "\n"
-                                  .join("period: {} status: {}"
-                                        .format(
-                                                value['time'],
-                                                value['status']
-                                                ) for value in ports['fail'])))
-        elif ports['status'] == StatusEnum.ERROR:
+            logging.error(
+                "Fail on port %s: %s\n",
+                port['port'], port['message'])
+            logging.error("Detail:\n")
+            for value in port['fail']:
+                logging.error("Period: %s, Status: %s",
+                              value['time'], value['status'])
+        elif port['status'] == StatusEnum.ERROR:
             final = 1
-            logging.error("Not able to connect on port {}."
-                          "{}"
-                          .format(ports['port'],
-                                  ports['message']))
+            logging.error("Not able to connect on port %s."
+                          "%s", port['port'], port['message'])
     if final:
         raise RuntimeError("TCP payload test fail!")
     else:
-        logging.info("Run TCP multi-connections test in {}"
-                     .format(run_time))
+        logging.info("Run TCP multi-connections test Passed!")
 
 
 def server(start_port, end_port):
@@ -123,13 +121,13 @@ def handle_port(port):
             server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             server_socket.listen()
 
-            logging.info("Server listening on port {}".format(port))
+            logging.info("Server listening on port %s", port)
 
             while True:
                 try:
                     conn, addr = server_socket.accept()
                     with conn:
-                        logging.info("Connected by {}.".format(addr))
+                        logging.info("Connected by %s.", addr)
                         while True:
                             data = conn.recv(4096)
                             if data:
@@ -137,11 +135,10 @@ def handle_port(port):
                             else:
                                 break
                 except Exception as e:
-                    logging.error("Error handling connection: {}".
-                                  format(str(e)))
+                    logging.error("Error handling connection: %s", str(e))
     except Exception as e:
-        logging.error("{}: An unexpected error occurred for port {}"
-                      .format(str(e), port))
+        logging.error("{}: An unexpected error occurred for port %s",
+                      str(e), port)
 
 
 def client(host, start_port, end_port, payload, start_time, results):
@@ -171,8 +168,8 @@ def client(host, start_port, end_port, payload, start_time, results):
     # Wait for all client threads to finish
     for thread in threads:
         thread.join()
-
-    generate_result(results, (datetime.now() - time))
+    logging.info("Running TCP multi-connections in %s", (datetime.now() - time))
+    check_result(results)
 
 
 def send_payload(host, port, payload, start_time, results):
@@ -182,7 +179,7 @@ def send_payload(host, port, payload, start_time, results):
     Args:
     - host (str): Server host.
     - port (int): Port to connect to.
-    - payload (str): Payload to send to the server.
+    - payload (int): Payload size in KB for the client.
     - start_time (datetime): Time until which the client should run.
     """
     # Retry connect to server port for 5 times.
@@ -195,11 +192,11 @@ def send_payload(host, port, payload, start_time, results):
                 # Set send buffer size to 4096
                 client_socket.setsockopt(socket.SOL_SOCKET,
                                          socket.SO_SNDBUF, 4096)
-                logging.info("Connect to port {}".format(port))
+                logging.info("Connect to port %s", port)
                 # Sleep until start time)
                 start_time = start_time - datetime.now()
                 time.sleep(start_time.total_seconds())
-                logging.info("Sending payload to port {}.".format(port))
+                logging.info("Sending payload to port %s.", port)
                 # Sending payload for 10 times
                 for x in range(10):
                     single_start = datetime.now()
@@ -223,15 +220,14 @@ def send_payload(host, port, payload, start_time, results):
                     else:
                         status_all[x] = {'time': single_end,
                                          'status': True}
-                logging.info("Received payload from {}.".
-                             format(server_host))
+                logging.info("Received payload from %s.", server_host)
                 client_socket.close()
                 break
         except socket.error as e:
-            logging.error("{} on {}".format(e, port))
+            logging.error("%s on %s", e, port)
             message = str(e)
         except Exception as e:
-            logging.error("{} on {}".format(e, port))
+            logging.error("%s on %s", e, port)
             message = str(e)
         time.sleep(3)
     results.append(format_output(port, message, status_all))
